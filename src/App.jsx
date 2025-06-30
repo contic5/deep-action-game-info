bc86f42375da777a9c86f9218374dc1d62d63ee2
CJ Conti <cjc2818cjc@gmail.com>
2025-06-30 14:14:22 -0400

    Added filter

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,29 @@ import {read_excel_file} from './read_files.js'
 function App() {
   function update_game_elements()
   {
-    const game_info_copy=[...game_info];
+    let game_info_copy=[...game_info];
+    if(filtering_mature_games)
+    {
+      game_info_copy=game_info_copy.filter(game=>game.Mature_Rating==false);
+    }
     const game_elements_temp=game_info_copy.map(game=><GameElement game={game} key={game.ID}></GameElement>);
     setGameElements(game_elements_temp);
+  }
+  function toggle_mature_games()
+  {
+    if(filtering_mature_games==true)
+    {
+      setFilteringMatureGames(false);
+    }
+    else
+    {
+      setFilteringMatureGames(true);
+    }
   }
 
   const [game_info,setGameInfo]=useState(null);
   const [game_elements,setGameElements]=useState([]);
+  const [filtering_mature_games,setFilteringMatureGames]=useState(true);
   useEffect(()=>{
     read_excel_file("./Game_Info.xlsx").then((response)=>
     {
@@ -28,11 +44,12 @@ function App() {
     {
       update_game_elements();
     }
-  },[game_info])
+  },[game_info,filtering_mature_games])
 
   return (
     <>
       <h1>Deep Action Games Info</h1>
+      <button onClick={toggle_mature_games} id="display_mature_games_button">{filtering_mature_games ? "Display Mature Games" :"Hide Mature Games"}</button>
       {game_elements}
     </>
   )
--- src/GameElement.jsx
+++ src/GameElement.jsx
@@ -23,9 +23,15 @@ function GameElement(props)
 
     const major_boss_embed=<iframe width="560" height="315" src={"https://www.youtube.com/embed/"+game.Major_Boss_Youtube_Src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>;
 
+    let display_mature_rating="";
+    if(game.Mature_Rating)
+    {
+        display_mature_rating=<h3>Rating: <b>Mature</b></h3>;
+    }
     return(<>
     <div className="gameElement">
     <h2>Game Element: {game.Game}</h2>
+    {display_mature_rating}
     <img src={game.Logo} className="gameLogo"></img>
     <h3>Game Info</h3>
     <p>{game.Short_Official_Description}</p>
